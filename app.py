ef0c19b0436ab326d98e57f0af5659ab5cf10f5f
Solomon Wilson <93185107+spartasolopolo@users.noreply.github.com>
2023-02-01 01:31:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from ibm_watson import LanguageTranslatorV4
+from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator, authenticator
 
 
